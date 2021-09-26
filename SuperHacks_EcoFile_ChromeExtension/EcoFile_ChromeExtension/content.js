e176b71e8b07bdf34a219ba85c7aed3e0dfeea9b
Chris Ren <daming112@gmail.com>
2021-09-26 15:10:07 -0400

    slight tweaks to chrome

--- SuperHacks_EcoFile_ChromeExtension/EcoFile_ChromeExtension/content.js
+++ SuperHacks_EcoFile_ChromeExtension/EcoFile_ChromeExtension/content.js
@@ -8,34 +8,64 @@ var actualCode = '('+ function modifyDOM() {
         var companyName = document.getElementsByClassName("jobs-unified-top-card__subtitle-primary-grouping mr2 t-black")[0].children[0].textContent.trim();
         console.log(companyName);
         
-        var num = 1;
+        async function () {
+            const data = JSON.stringify({
+              query: `{
+            characters(isMonster:true) {
+              name
+              episode {
+                name
+              }
+            }
+          }`,
+            });
+        
+            const response = await fetch(
+              'https://biggs.stepzen.net/scoobydoo/scoobydoo/__graphql',
+              {
+                method: 'post',
+                body: data,
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Content-Length': data.length,
+                  Authorization:
+                    'Apikey DONOTSENDAPIKEYS',
+                },
+              }
+            );
+        
+            const characterData = await response.json();
+
+        var num = data;
+
         var div=document.createElement("div");
 
         div.style = "font-family: 'Courier New', monospace; font-weight: bold; font-size: 20px; "
         if (num) {
             if (num > 4) {
-                div.innerText="Susmeter Score: " + num;
+                div.innerText="Ecofile Susmeter Rating: " + num;
                 div.style.color = "green";
             } else if (num > 3) {
-                div.innerText="Susmeter Score: " + num;
+                div.innerText="Ecofile Susmeter Rating: " + num;
                 div.style.color = "greenyellow";
             } else if (num > 2) {
-                div.innerText="Susmeter Score: " + num;
+                div.innerText="Ecofile Susmeter Rating: " + num;
                 div.style.color = "yellow";
             } else if (num > 1) {
-                div.innerText="Susmeter Score: " + num;
+                div.innerText="Ecofile Susmeter Rating: " + num;
                 div.style.color = "orange";
             } else {
-                div.innerText="Susmeter Score: " + num;
+                div.innerText="Ecofile Susmeter Rating: " + num;
                 div.style.color = "red";
             }
         } else {
-            div.innerText="Susmeter Score: Not Found ඞ";
+            div.innerText="Ecofile Susmeter Rating: Not Found ඞ";
             div.style.color = "silver";
         }
         
         const nextDiv = document.getElementsByClassName("mt5 mb2")[0];
         document.getElementsByClassName("p5")[0].insertBefore(div, nextDiv);
+
     } else if (site.includes("google.com/search")) {
         console.log('Tab script:');
         var companyName;
@@ -55,23 +85,23 @@ var actualCode = '('+ function modifyDOM() {
             div.style = "font-family: 'Courier New', monospace; font-weight: bold; font-size: 15px; "
             if (num) {
                 if (num > 4) {
-                    div.innerText="Susmeter Score: " + num;
+                    div.innerText="Ecofile Susmeter Rating: " + num;
                     div.style.color = "green";
                 } else if (num > 3) {
-                    div.innerText="Susmeter Score: " + num;
+                    div.innerText="Ecofile Susmeter Rating: " + num;
                     div.style.color = "greenyellow";
                 } else if (num > 2) {
-                    div.innerText="Susmeter Score: " + num;
+                    div.innerText="Ecofile Susmeter Rating: " + num;
                     div.style.color = "yellow";
                 } else if (num > 1) {
-                    div.innerText="Susmeter Score: " + num;
+                    div.innerText="Ecofile Susmeter Rating: " + num;
                     div.style.color = "orange";
                 } else {
-                    div.innerText="Susmeter Score: " + num;
+                    div.innerText="Ecofile Susmeter Rating: " + num;
                     div.style.color = "red";
                 }
             } else {
-                div.innerText="Susmeter Score: Not Found ඞ";
+                div.innerText="Ecofile Susmeter Rating: Not Found ඞ";
                 div.style.color = "silver";
             }
             
@@ -79,37 +109,6 @@ var actualCode = '('+ function modifyDOM() {
             boxes[i].insertBefore(div, nextDiv);
                         
         }
-
-        /*
-        var num = 1;
-        var div=document.createElement("div");
-
-        div.style = "font-family: 'Courier New', monospace; font-weight: bold; font-size: 20px; "
-        if (num) {
-            if (num > 4) {
-                div.innerText="Susmeter Score: " + num;
-                div.style.color = "green";
-            } else if (num > 3) {
-                div.innerText="Susmeter Score: " + num;
-                div.style.color = "greenyellow";
-            } else if (num > 2) {
-                div.innerText="Susmeter Score: " + num;
-                div.style.color = "yellow";
-            } else if (num > 1) {
-                div.innerText="Susmeter Score: " + num;
-                div.style.color = "orange";
-            } else {
-                div.innerText="Susmeter Score: " + num;
-                div.style.color = "red";
-            }
-        } else {
-            div.innerText="Susmeter Score: Not Found";
-            div.style.color = "silver";
-        }
-        
-        const nextDiv = document.getElementsByClassName("mt5 mb2")[0];
-        document.getElementsByClassName("p5")[0].insertBefore(div, nextDiv);
-        */
     } else {
         console.log("Program not optimized for this Page Type")
     }

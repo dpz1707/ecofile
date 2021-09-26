29d4a1fb71fbdc4a6676b97ca053d3533ebce21c
dpz1707 <90921421+dpz1707@users.noreply.github.com>
2021-09-26 12:07:03 -0400

    Add files via upload

---
+++ src/components/Planet.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { useSubscription, useMutation, gql } from "@apollo/client";
+import { List, ListItem } from "./shared/List";
+import { Badge } from "./shared/Badge";
+import InputForm from "./shared/InputForm";
+
+const PLANET = gql`
+  subscription Planet($id: uuid!) {
+    planets_by_pk(id: $id) {
+      id
+      name
+      cuisine
+        reviews {
+        id
+        body
+      }
+    }
+  }
+`;
+
+const ADD_REVIEW = gql`
+  mutation($body: String!, $id: uuid!) {
+    AddFearlessReview(body: $body, id: $id) {
+      affected_rows
+    }
+  }
+`;
+
+
+const Planet = ({
+    match: {
+        params: { id },
+    },
+}) => {
+    const [inputVal, setInputVal] = useState("");
+    if (!id) {
+        console.log("id not found")
+    }
+    const { loading, error, data } = useSubscription(PLANET, {
+
+        variables: { id },
+    });
+    const [addReview] = useMutation(ADD_REVIEW);
+
+        if (loading) return <p>Loading ...</p>;
+        console.log({ data })
+        console.log({loading, error, data})
+        if (error) return <p>{data}</p>;
+
+        const { name, cuisine, reviews } = data.planets_by_pk;
+
+        return (
+            <div>
+                <h3>
+                    {name} <Badge>{cuisine}</Badge>
+                </h3>
+                <InputForm
+                    inputVal={inputVal}
+                    onChange={(e) => setInputVal(e.target.value)}
+                    onSubmit={() => {
+                        addReview({ variables: { id, body: inputVal } })
+                            .then(() => setInputVal(""))
+                            .catch((e) => {
+                                setInputVal(e.message);
+                            });
+                    }}
+                    buttonText="Submit"
+                />
+                <List>
+                    {reviews.map((review) => (
+                        <ListItem key={review.id}>{review.body}</ListItem>
+                    ))}
+                </List>
+            </div>
+        );
+    
+};
+
+export default Planet;
